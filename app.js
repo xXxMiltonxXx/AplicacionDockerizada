f98b7eee1f887216dcefd1f2611d4187edb470d8
JTucker04 <jatuker@hotmail.es>
2024-07-25 21:35:28 -0500

    grupo11

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-// Nombre de integrantes:
-// Aleejandro Bueno Peralta
-// Samandiego Leidy
+/* Nombre de integrantes:
+Anthony Valencia Vélez
+Julio Tucker Bermúdez
+Francisco Villon
+Issac Sornoza
+*/
+
 const express = require('express');
 const app = express();
 const port = 3000;
